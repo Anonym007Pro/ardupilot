ee6fed2d3f9e4ac8de78d1c9de84d7ddee29e033
Anonym007Pro <86276002+Anonym007Pro@users.noreply.github.com>
2022-10-28 18:55:12 +0200

    Update AP_Motors6DOF.cpp
    ROV_0

--- libraries/AP_Motors/AP_Motors6DOF.cpp
+++ libraries/AP_Motors/AP_Motors6DOF.cpp
@@ -181,11 +181,11 @@ void AP_Motors6DOF::setup_motors(motor_frame_class frame_class, motor_frame_type
     case SUB_FRAME_SIMPLEROV_5:
     default:
         _frame_class_string = "DEFAULT";
-        add_motor_raw_6dof(AP_MOTORS_MOT_1,    -2.0f,          -2.0f,              2.0f,           0,                  2.0f,               0,              1);
-        add_motor_raw_6dof(AP_MOTORS_MOT_2,     2.0f,          -2.0f,             -2.0f,           0,                  2.0f,               0,              2);
-        add_motor_raw_6dof(AP_MOTORS_MOT_3,    -2.0f,          -2.0f,             -2.0f,           0,                  2.0f,               0,              3);
-        add_motor_raw_6dof(AP_MOTORS_MOT_4,     2.0f,           2.0f,              2.0f,           0,                  2.0f,               0,              4);
-        add_motor_raw_6dof(AP_MOTORS_MOT_5,     0,              0,                 0.2f,           2.0f,               0,                  0,              5);
+        add_motor_raw_6dof(AP_MOTORS_MOT_1,        0,              0,              2.0f,           0,                  2.0f,               0,              1);
+        add_motor_raw_6dof(AP_MOTORS_MOT_2,        0,              0,             -2.0f,           0,                  2.0f,               0,              2);
+        add_motor_raw_6dof(AP_MOTORS_MOT_3,        0,          -2.0f,                 0,        2.0f,                     0,               0,              3);
+        add_motor_raw_6dof(AP_MOTORS_MOT_4,        0,           2.0f,                 0,        2.0f,                     0,               0,              4);
+        add_motor_raw_6dof(AP_MOTORS_MOT_5,        0,              0,                 0,           0,                     0,            2.0f,              5);
         break;
     
     }
